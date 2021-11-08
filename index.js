707bee077bcf25a09b297e66e83f8f3a17135e76
NelsonT19 <nelsont408@gmail.com>
2021-11-08 09:56:42 -0100

    app listen

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
+const PORT = 1337;
 //cliente so tem acesso ao public
 app.use(express.static('./public'))
-
 
 
 app.get('/', function (req, res)  {
  res.sendFile(path.join(__dirname,'/public/index.html'))
 })
 
-//app.get('/:Nelson', function (req, res)  {
- //   res.send(`Olá ${req.params.Nelson}`)
- // })
 
-app.listen(1337)
+app.listen(PORT, function(){
+   console.log(`Server is running on PORT: ` + PORT);
+});
